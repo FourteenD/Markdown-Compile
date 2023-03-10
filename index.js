8c49a65279bd923ded907c0ebc89b4033cc25080
FourteenD <2899385645@qq.com>
2023-03-11 00:56:21 +0800

    vault backup: 2023-03-11 00:56:18
    Affected files:
    仓库/Markdown-Compile

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // markdomn-compile
 
-// 读取文件
+
 const fs = require('fs');
 const path = require('path');
 const md = fs.readFileSync(path.resolve(__dirname, 'test.md'), 'utf-8');
 
-// 词法分析
-
-// 语法分析
+// 规则
+const rules = [
+  {
+    name: 'Heading',
+    reg: /^#{1,6}\s+(.*)$/,
+    replace: (match, p1) => `<h${match.split(' ')[0].length}>${p1}</h${match.split(' ')[0].length}>`,
+  },
+];
