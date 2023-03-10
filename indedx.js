5f3d02b4f9e96a323af144d8c4912ab58d6549e8
FourteenD <2899385645@qq.com>
2023-03-11 00:39:52 +0800

    vault backup: 2023-03-11 00:39:50
    Affected files:

--- indedx.js
+++ indedx.js
@@ -1,1 +1,13 @@
-// M
+// markdomn-compile
+
+// 1. 读取文件
+const fs = require('fs');
+const path = require('path');
+const md = fs.readFileSync(path.resolve(__dirname, 'test.md'), 'utf-8');
+
+// 2.规则
+const rules = [
+  // 标题
+  {
+  }
+]
