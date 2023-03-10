3e6d6f7a784f7dd302bad552522a5f5c342227b0
FourteenD <2899385645@qq.com>
2023-03-11 01:21:01 +0800

    vault backup: 2023-03-11 01:20:59
    Affected files:

--- index.js
+++ index.js
@@ -7,13 +7,83 @@ const md = fs.readFileSync(path.resolve(__dirname, 'test.md'), 'utf-8');
 
 // 规则
 const rules = [
+  // 标题
   {
-    name: 'Heading',
-    reg: /^#{1,6}\s+(.*)$/,
-    replace: (match, p1) => {
-      const level = match.match(/^#{1,6}/)[0].length;
-      return `<h${level}>${p1}</h${level}>`;
-    },
+    name: 'heading',
+    reg: /^# .+$/gm,
+    replace: (content) => {
+      const level = content.match(/^#+/)[0].length;
+      const text = content.replace(/^#+/, '');
+      return `<h${level}>${text}</h${level}>`;
+    }
+  },
+  // 粗体
+  {
+    name: 'bold',
+    reg: /\*\*[^*]+\*\*/g,
+    replace: (content) => {
+      const text = content.replace(/\*\*/g, '');
+      return `<strong>${text}</strong>`;
+    }
+  },
+  // 斜体
+  {
+    name: 'italic',
+    reg: /\*[^*]+\*/g,
+    replace: (content) => {
+      const text = content.replace(/\*/g, '');
+      return `<em>${text}</em>`;
+    }
+  },
+  // 链接
+  {
+    name: 'link',
+    reg: /\[[^\]]+\]\([^\)]+\)/g,
+    replace: (content) => {
+      const text = content.match(/\[([^\]]+)\]/)[1];
+      const href = content.match(/\(([^\)]+)\)/)[1];
+      return `<a href="${href}">${text}</a>`;
+    }
+  },
+  // 代码
+  {
+    name: 'code',
+    reg: /`[^`]+`/g,
+    replace: (content) => {
+      const text = content.replace(/`/g, '');
+      return `<code>${text}</code>`;
+    }
+  },
+  // 图片
+  {
+    name: 'image',
+    reg: /!\[[^\]]+\]\([^\)]+\)/g,
+    replace: (content) => {
+      const text = content.match(/\[([^\]]+)\]/)[1];
+      const src = content.match(/\(([^\)]+)\)/)[1];
+      return `<img src="${src}" alt="${text}">`;
+    }
+  },
+  // 列表
+  {
+    name: 'list',
+    reg: /^(\s*)(\d+\.|-) .+$/gm,
+    replace: (content) => {
+      const space = content.match(/^(\s*)/)[0];
+      const level = space.length / 2;
+      const text = content.replace(/^(\s*)(\d+\.|-) /, '');
+      const isOrder = content.match(/^(\s*)(\d+\.|-)/)[2] === '.';
+      const tag = isOrder ? 'ol' : 'ul';
+      return `<${tag} level="${level}"><li>${text}</li></${tag}>`;
+    }
+  },
+  // 段落
+  {
+    name: 'paragraph',
+    reg: /^[^#*!-].+$/gm,
+    replace: (content) => {
+      return `<p>${content}</p>`;
+    }
   },
 ];
 
@@ -64,4 +134,4 @@ function compiler(md) {
   return html;
 }
 
-console.log(compiler(md));
+fs.writeFileSync(path.resolve(__dirname, 'test.html'), compiler(md));
