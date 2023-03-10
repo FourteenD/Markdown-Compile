1b0fdc610968930d66c979b8dd4a163e6588aa31
FourteenD <2899385645@qq.com>
2023-03-11 00:52:52 +0800

    vault backup: 2023-03-11 00:52:51
    Affected files:

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
 // markdomn-compile
 
-// 1. 读取文件
+// 读取文件
 const fs = require('fs');
 const path = require('path');
 const md = fs.readFileSync(path.resolve(__dirname, 'test.md'), 'utf-8');
 
-// 2.规则
-const rules = [
-  // 标题
-  {
-    name: 'header',
-    reg: /^#{1,6}\s+(.*)$/,
-    cb: (str, $1) => {
-      const level = str.match(/^#+/)[0].length;
-      return `<h${level}>${$1}</h${level}>`;
-    }
-  },
-];
+// 词法分析
